9fd20facd9e0d3d341b5513e83a2efe22ba44ef4
Gijsbert van Dalen <43387429+Gijsbertdalen@users.noreply.github.com>
2018-10-28 13:52:59 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -9,7 +9,7 @@ root = lxml.html.fromstring(html) # turn our HTML into an lxml object
 tds = root.cssselect('td') # get all the <td tags
 
 for td in tds:
-  print td.tag # just the text inside the HTML tag
+  print td.attrib['href'] # just the text inside the HTML tag
 
 for td in tds:
   record = { "td" : td.text_content() } # column name and value
