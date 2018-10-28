2f9249ac38ed7da1009961b005b7ad2149534cb7
Gijsbert van Dalen <43387429+Gijsbertdalen@users.noreply.github.com>
2018-10-28 13:57:30 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -6,11 +6,11 @@ import lxml.html
 
 root = lxml.html.fromstring(html) # turn our HTML into an lxml object
 
-tds = root.cssselect('td') # get all the <td tags
+tds = root.cssselect('tr') # get all the <td tags
 
 for td in tds:
-  print td.text_content() # just the text inside the HTML tag
+  print tr.text_content() # just the text inside the HTML tag
 
-for td in tds:
-  record = { "td" : td.text_content() } # column name and value
-  scraperwiki.sqlite.save(["td"], record) # save the records one by one
+for tr in tds:
+  record = { "tr" : tr.text_content() } # column name and value
+  scraperwiki.sqlite.save(["tr"], record) # save the records one by one
